844784370092c144a6f61d50b843f0280fb90cc2
Elio Diaz Uyola <ediasulloa@gmail.com>
2023-09-25 10:32:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'date-dog',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
